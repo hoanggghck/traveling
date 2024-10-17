5592d8b81f0ccfaab4084514e7663535808efb60
Pham Duy Hoang <97928971+kng-duyhoang@users.noreply.github.com>
2024-10-17 10:47:15 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BackgroundWrapper, Container, GlobalStyle, Wrapper } from './style'
 import { useState } from 'react'
 import ScheduleContent from '@/components/schedule';
 import coverimage from '@/assets/images/cover.jpg';
-import Sumary from '@/components/Sumary';
+import Sumary from './components/sumary';
 
 function App() {
   const [activeDay, setActiveDay] = useState(1);
